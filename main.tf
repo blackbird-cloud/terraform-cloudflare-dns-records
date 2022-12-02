354a7464bdfd7aae9be17fdaecd35952243a58fb
Joeri Malmberg <joeri.malmberg@blackbird.cloud>
2022-12-02 14:24:01 +0100

    Fix: Fixed a copy paste error on the proxied attribute for each record.

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
-terraform {
-  required_version = ">= 1"
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "3.16"
-    }
-  }
+data "cloudflare_zone" "this" {
+  count = var.zone_name != "" ? 1 : 0
+  name  = var.zone_name
+}
+
+resource "cloudflare_record" "records" {
+  for_each = { for record in var.records : record.name => record }
+
+  zone_id         = try(each.value.zone_id, data.cloudflare_zone.this[0].id)
+  name            = each.value.name
+  value           = each.value.value
+  type            = each.value.type
+  ttl             = try(each.value.ttl, 300)
+  priority        = try(each.value.priority, null)
+  proxied         = try(each.value.proxied, false)
+  allow_overwrite = var.allow_overwrite
 }
